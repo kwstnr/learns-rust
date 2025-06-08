3e1e5346adf7660c83c1c6e36bbca4388a46bc50
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-06-08 17:46:25 +0200

    refactor(search): return tuple of line and line number

--- minigrep/src/search/mod.rs
+++ minigrep/src/search/mod.rs
@@ -1,9 +1,11 @@
-pub fn search_string(query: &str, content: &str) -> Vec<String> {
+pub fn search_string(query: &str, content: &str) -> Vec<(u32, String)> {
     let mut results = Vec::new();
+    let mut index = 1;
     for line in content.lines() {
         if line.contains(query) {
-            results.push(line.to_string());
+            results.push((index, line.to_string()));
         }
+        index = index + 1;
     }
     results
 }
