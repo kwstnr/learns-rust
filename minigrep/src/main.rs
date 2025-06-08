154b9d8f385c0358e1d1f5eb120f39a013907ac8
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-06-08 17:49:41 +0200

    feat(minigrep): use search and log results

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use minigrep::args_parser::Args;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,4 +20,15 @@ fn main() {
             std::process::exit(1);
         }
     };
+
+    let results = search::search_string(&args.query, &file_content);
+
+    if results.is_empty() {
+        println!("No matches found for '{}'", args.query);
+    } else {
+        println!("Matches found for '{}':", args.query);
+        for result in results {
+            println!("{}: {}", result.line_number, result.content);
+        }
+    }
 }
